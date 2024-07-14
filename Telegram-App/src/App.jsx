5bd85a893db9805d1d80a64f5f31758547669353
RohanGorla <rohangorla2010@gmail.com>
2024-07-15 01:43:10 +0530

    add chat messages and go to particular chat feature

--- Telegram-App/src/App.jsx
+++ Telegram-App/src/App.jsx
@@ -4,44 +4,95 @@ import "./App.css";
 function App() {
   const [page, setPage] = useState([]);
   const [message, setMessage] = useState([]);
+  const [chatId, setChatId] = useState();
+  const [show, setShow] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
-      const page_response = await fetch(
-        "https://devapi.beyondchats.com/api/get_all_chats?page=1"
-      );
-      const chat_response = await fetch(
-        "https://devapi.beyondchats.com/api/get_chat_messages?chat_id=3889"
-      );
-      const page_data = await page_response.json();
-      const chat_data = await chat_response.json();
-      setPage(page_data.data.data);
-      console.log(page_data.data.data);
-      setMessage(chat_data.data);
-      console.log(chat_data.data);
+      const data = [];
+      for (let i = 1; i <= 10; i++) {
+        const page_response = await fetch(
+          `https://devapi.beyondchats.com/api/get_all_chats?page=${i}`
+        );
+        const page_data = await page_response.json();
+        data.push(page_data);
+      }
+      console.log("data is ->", data);
+      setPage(data);
     }
     fetchData();
   }, []);
 
+  useEffect(() => {
+    async function fetchData() {
+      if (chatId) {
+        const chat_response = await fetch(
+          `https://devapi.beyondchats.com/api/get_chat_messages?chat_id=${chatId}`
+        );
+        const chat_data = await chat_response.json();
+        setMessage(chat_data.data);
+        console.log("chat data -> ", chat_data.data);
+      }
+    }
+    fetchData();
+  }, [chatId]);
+
   return (
     <div className="Telegram-main">
       <nav className="navbar">
-        <div className="hamburger-button"></div>
+        <div className="hamburger-button">
+          <span className="hamburger"></span>
+        </div>
         <div className="logo"></div>
         <div className="search-button"></div>
       </nav>
       <section className="main">
         <div className="chat-list">
           {page.map((page) => {
+            return page.data.data.map((page, index) => {
+              if (page.creator.name) {
+                return (
+                  <div
+                    style={{ border: "white 1px solid", margin: "10px 5px" }}
+                    className={show ? "list" : "d-none"}
+                    key={index}
+                    onClick={() => {
+                      setChatId(page.id);
+                      setShow(!show);
+                    }}
+                  >
+                    <h3>{page.creator.id}</h3>
+                    <h2>{page.creator.name}</h2>
+                  </div>
+                );
+              }
+            });
+          })}
+        </div>
+        <div className="chat-message">
+          <div className="back-button">
+            <button
+              onClick={() => {
+                setShow(!show);
+              }}
+              className={show ? "d-none hide" : "hide"}
+            >
+              back
+            </button>
+          </div>
+          {message.map((chat, index) => {
             return (
-              <div className="list" key={page.created_at}>
-                <h3>{page.creator.id}</h3>
-                <h2>{page.creator.name}</h2>
+              <div style={{ margin: "15px 5px" }} key={index}>
+                {chat.sender.name ? (
+                  <h2>{chat.sender.name}</h2>
+                ) : (
+                  <h2>Unknown User</h2>
+                )}
+                <p>{chat.message}</p>
               </div>
             );
           })}
         </div>
-        <div className="chat-message">{}</div>
       </section>
     </div>
   );
