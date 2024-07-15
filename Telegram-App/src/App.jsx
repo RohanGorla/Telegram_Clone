a4fe6855a0f9de4b792066f4bd559d564a10f942
RohanGorla <rohangorla2010@gmail.com>
2024-07-15 16:01:15 +0530

    update chat layout and add profile picture letters

--- Telegram-App/src/App.jsx
+++ Telegram-App/src/App.jsx
@@ -41,7 +41,7 @@ function App() {
 
   return (
     <div className={mode ? "Telegram-main light" : "Telegram-main dark"}>
-      <nav className="navbar">
+      <nav className={mode ? "navbar nav-light" : "navbar nav-dark"}>
         <div
           className="hamburger-button"
           onClick={() => {
@@ -51,7 +51,7 @@ function App() {
           <span className="hamburger"></span>
         </div>
         <div className="logo">
-          <h1 style={{ textAlign: "center" }}>TELEGRAM</h1>
+          <h1 style={{ textAlign: "center", color: "white" }}>TELEGRAM</h1>
         </div>
       </nav>
       <section
@@ -66,7 +66,10 @@ function App() {
               setSidebarShow(!sidebarShow);
             }}
           >
-            back
+            <img
+              style={{ width: "100%" }}
+              src="https://cdn.iconscout.com/icon/free/png-256/free-back-arrow-1767515-1502579.png"
+            ></img>
           </div>
           <div
             className="light-dark-button"
@@ -74,30 +77,85 @@ function App() {
               setMode(!mode);
             }}
           >
-            mode
+            <img
+              style={{ width: "100%" }}
+              src={
+                mode
+                  ? `https://t4.ftcdn.net/jpg/05/78/50/85/360_F_578508513_Vpl7hrU2NpSvDBzvDVi8W75zOTxHvTLI.webp`
+                  : `https://static.thenounproject.com/png/4808961-200.png`
+              }
+            ></img>
           </div>
         </div>
 
         <div className="sidebar-options">
-          <p style={{margin:'8px 0', textAlign:'center'}}>My Profile</p>
-          <p style={{margin:'8px 0', textAlign:'center'}}>New Group</p>
-          <p style={{margin:'8px 0', textAlign:'center'}}>Contacts</p>
-          <p style={{margin:'8px 0', textAlign:'center'}}>Calls</p>
-          <p style={{margin:'8px 0', textAlign:'center'}}>People Nearby</p>
-          <p style={{margin:'8px 0', textAlign:'center'}}>saved Messages</p>
-          <p style={{margin:'8px 0', textAlign:'center'}}>Settings</p>
-          <p style={{margin:'8px 0', textAlign:'center'}}>Invite Friends</p>
-          <p style={{margin:'8px 0', textAlign:'center'}}>Telegram Features</p>
+          <p
+            style={{ margin: "8px 0", textAlign: "center", cursor: "pointer" }}
+          >
+            My Profile
+          </p>
+          <p
+            style={{ margin: "8px 0", textAlign: "center", cursor: "pointer" }}
+          >
+            New Group
+          </p>
+          <p
+            style={{ margin: "8px 0", textAlign: "center", cursor: "pointer" }}
+          >
+            Contacts
+          </p>
+          <p
+            style={{ margin: "8px 0", textAlign: "center", cursor: "pointer" }}
+          >
+            Calls
+          </p>
+          <p
+            style={{ margin: "8px 0", textAlign: "center", cursor: "pointer" }}
+          >
+            People Nearby
+          </p>
+          <p
+            style={{ margin: "8px 0", textAlign: "center", cursor: "pointer" }}
+          >
+            saved Messages
+          </p>
+          <p
+            style={{ margin: "8px 0", textAlign: "center", cursor: "pointer" }}
+          >
+            Settings
+          </p>
+          <p
+            style={{ margin: "8px 0", textAlign: "center", cursor: "pointer" }}
+          >
+            Invite Friends
+          </p>
+          <p
+            style={{ margin: "8px 0", textAlign: "center", cursor: "pointer" }}
+          >
+            Telegram Features
+          </p>
         </div>
       </section>
-      <section className="main">
+      <section
+        className="main"
+        onClick={() => {
+          setSidebarShow(false);
+        }}
+      >
         <div className="chat-list">
           {page.map((page) => {
             return page.data.data.map((page, index) => {
               if (page.creator.name) {
+                const colors = ["red", "green", "orange", "brown", "steelblue"];
+                const random = Math.floor(Math.random() * 5);
+                const color = colors[random];
+                console.log(color);
                 return (
                   <div
-                    style={{ border: "white 1px solid", margin: "10px 5px", cursor:'pointer' }}
+                    style={{
+                      margin: "10px 5px",
+                      cursor: "pointer",
+                    }}
                     className={show ? "list" : "d-none"}
                     key={index}
                     onClick={() => {
@@ -105,8 +163,31 @@ function App() {
                       setShow(!show);
                     }}
                   >
-                    <h3>{page.creator.id}</h3>
-                    <h2>{page.creator.name}</h2>
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <div
+                        style={{
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                          width: "40px",
+                          height: "40px",
+                          marginRight: "10px",
+                          borderRadius: "50%",
+                          backgroundColor: color,
+                        }}
+                      >
+                        <h2>{page.creator.name.charAt(0).toUpperCase()}</h2>
+                      </div>
+                      {/* <h3>{page.creator.id}</h3> */}
+                      <h2>{page.creator.name}</h2>
+                    </div>
+                    <div
+                      style={{
+                        height: "3px",
+                        backgroundColor: "black",
+                        marginTop: "10px",
+                      }}
+                    ></div>
                   </div>
                 );
               }
@@ -114,28 +195,45 @@ function App() {
           })}
         </div>
         <div className="chat-message">
-          <div className="back-button">
-            <button
-              onClick={() => {
-                setShow(!show);
-              }}
-              className={show ? "d-none hide" : "hide"}
-            >
-              back
-            </button>
+          <div
+            onClick={() => {
+              setShow(!show);
+            }}
+            className={
+              show ? "chat-back-button d-none hide" : "chat-back-button hide"
+            }
+          >
+            <img
+              style={{ width: "100%" }}
+              src="https://cdn.iconscout.com/icon/free/png-256/free-back-arrow-1767515-1502579.png"
+            ></img>
           </div>
-          {message.map((chat, index) => {
-            return (
-              <div style={{ margin: "15px 5px" }} key={index}>
-                {chat.sender.name ? (
-                  <h2>{chat.sender.name}</h2>
-                ) : (
-                  <h2>Unknown User</h2>
-                )}
-                <p>{chat.message}</p>
-              </div>
-            );
-          })}
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            {message.map((chat, index) => {
+              return (
+                <div
+                  style={
+                    chat.sender_id == 1
+                      ? {
+                          margin: "15px 5px",
+                          backgroundColor: "slategrey",
+                          maxWidth: "60%",
+                          alignSelf: "end",
+                        }
+                      : { margin: "15px 5px", maxWidth: "60%" }
+                  }
+                  key={index}
+                >
+                  {chat.sender.name ? (
+                    <h2>{chat.sender.name}</h2>
+                  ) : (
+                    <h2>Unknown User</h2>
+                  )}
+                  <p>{chat.message}</p>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </section>
     </div>
